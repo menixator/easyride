489b1f8341187175221b9835f757e25d212ff47a
Ahmed Miljau <ahmed.miljau@gmail.com>
2020-06-27 18:11:15 +0500

    Finish up ride's model class

--- src/java/com/easyride/models/Ride.java
+++ src/java/com/easyride/models/Ride.java
@@ -1,7 +1,8 @@
 package com.easyride.models;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
-
 
 public class Ride {
 
@@ -39,11 +40,119 @@ public class Ride {
     }
 
     private int id;
-    private Timestamp startTime;
-    private Timestamp endTime;
-    private RideStatus status;
-
+    private int userId;
     private int driverId;
-    private int customerId;
+    private RideStatus status;
+    private double pickupLocationLongitude;
+    private double pickupLocationLatitiude;
+    private double destinationLongitude;
+    private double destinationLatitude;
+    private double fare;
+    private Timestamp requestedTimestamp;
+    private Timestamp endTimestamp;
+
+    public static Ride fromResultSet(ResultSet set) throws SQLException {
+        Ride ride = new Ride();
+        ride.setId(set.getInt("id"));
+        ride.setUserId(set.getInt("userId"));
+        ride.setDriverId(set.getInt("driverId"));
+        ride.setStatus(RideStatus.rideStatusFromString(set.getString("status")));
+        ride.setPickupLocationLongitude(set.getDouble("pickupLocationLongitude"));
+        ride.setPickupLocationLatitiude(set.getDouble("pickupLocationLatitiude"));
+        ride.setDestinationLongitude(set.getDouble("destinationLongitude"));
+        ride.setDestinationLatitude(set.getDouble("destinationLatitude"));
+        ride.setFare(set.getDouble("fare"));
+        ride.setRequestedTimestamp(set.getTimestamp("requestedTimestamp"));
+        ride.setEndTimestamp(set.getTimestamp("endTimestamp"));
+        return ride;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getDriverId() {
+        return driverId;
+    }
+
+    public void setDriverId(int driverId) {
+        this.driverId = driverId;
+    }
+
+    public RideStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(RideStatus status) {
+        this.status = status;
+    }
+
+    public double getPickupLocationLongitude() {
+        return pickupLocationLongitude;
+    }
+
+    public void setPickupLocationLongitude(double pickupLocationLongitude) {
+        this.pickupLocationLongitude = pickupLocationLongitude;
+    }
+
+    public double getPickupLocationLatitiude() {
+        return pickupLocationLatitiude;
+    }
+
+    public void setPickupLocationLatitiude(double pickupLocationLatitiude) {
+        this.pickupLocationLatitiude = pickupLocationLatitiude;
+    }
+
+    public double getDestinationLongitude() {
+        return destinationLongitude;
+    }
+
+    public void setDestinationLongitude(double destinationLongitude) {
+        this.destinationLongitude = destinationLongitude;
+    }
+
+    public double getDestinationLatitude() {
+        return destinationLatitude;
+    }
+
+    public void setDestinationLatitude(double destinationLatitude) {
+        this.destinationLatitude = destinationLatitude;
+    }
+
+    public double getFare() {
+        return fare;
+    }
+
+    public void setFare(double fare) {
+        this.fare = fare;
+    }
+
+    public Timestamp getRequestedTimestamp() {
+        return requestedTimestamp;
+    }
+
+    public void setRequestedTimestamp(Timestamp requestedTimestamp) {
+        this.requestedTimestamp = requestedTimestamp;
+    }
+
+    public Timestamp getEndTimestamp() {
+        return endTimestamp;
+    }
+
+    public void setEndTimestamp(Timestamp endTimestamp) {
+        this.endTimestamp = endTimestamp;
+    }
 
 }
