f767e1b0df21f0738b8b7d116eeb30859b5c0f7c
Ahmed Miljau <ahmed.miljau@gmail.com>
2020-06-28 16:31:10 +0500

    Add a routine to update ride information

--- src/java/com/easyride/dao/RideDao.java
+++ src/java/com/easyride/dao/RideDao.java
@@ -37,4 +37,27 @@ public class RideDao extends BaseDao {
             return false;
         }
     }
+
+    public static boolean updateRide(Ride ride) {
+        try {
+            Connection con = getConnection();
+            // Only one row will be returned because there is a unique constraint on email.
+            PreparedStatement statement = con.prepareStatement("UPDATE rides userId=?, driverId=?, status=?, pickupLocationLongitude=?, pickupLocationLatitiude=?, destinationLongitude=?, destinationLatitude=?, fare=?, requestedTimestamp=?, endTimestamp=? WHERE id =?");
+            statement.setInt(1, ride.getUserId());
+            statement.setInt(2, ride.getDriverId());
+            statement.setString(3, ride.getStatus().toString());
+            statement.setDouble(4, ride.getPickupLocationLongitude());
+            statement.setDouble(5, ride.getPickupLocationLatitiude());
+            statement.setDouble(6, ride.getDestinationLongitude());
+            statement.setDouble(7, ride.getDestinationLatitude());
+            statement.setDouble(8, ride.getFare());
+            statement.setTimestamp(9, ride.getRequestedTimestamp());
+            statement.setTimestamp(10, ride.getEndTimestamp());
+            statement.setInt(11, ride.getId());
+            statement.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            return false;
+        }
+    }
 }
