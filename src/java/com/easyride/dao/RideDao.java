527ac288668e6c06b779f0028b1e287611b3973b
Ahmed Miljau <ahmed.miljau@gmail.com>
2020-06-28 16:15:28 +0500

    Add a routine to create a ride

---
+++ src/java/com/easyride/dao/RideDao.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.easyride.dao;
+
+import com.easyride.models.Ride;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author miljau_a
+ */
+public class RideDao extends BaseDao {
+
+    public static boolean createRide(Ride ride) {
+        try {
+            Connection con = getConnection();
+            // Only one row will be returned because there is a unique constraint on email.
+            PreparedStatement statement = con.prepareStatement("INSERT INTO rides(userId, driverId, status, pickupLocationLongitude, pickupLocationLatitiude, destinationLongitude, destinationLatitude, fare, requestedTimestamp, endTimestamp) VALUES(?  ,?  ,?  ,?  ,?  ,?  ,?  ,?  ,?  ,?)");
+            statement.setInt(1, ride.getUserId());
+            statement.setInt(2, ride.getDriverId());
+            statement.setString(3, ride.getStatus().toString());
+            statement.setDouble(4, ride.getPickupLocationLongitude());
+            statement.setDouble(5, ride.getPickupLocationLatitiude());
+            statement.setDouble(6, ride.getDestinationLongitude());
+            statement.setDouble(7, ride.getDestinationLatitude());
+            statement.setDouble(8, ride.getFare());
+            statement.setTimestamp(9, ride.getRequestedTimestamp());
+            statement.setTimestamp(10, ride.getEndTimestamp());
+            statement.execute();
+            return true;
+        } catch (SQLException ex) {
+            return false;
+        }
+    }
+}
